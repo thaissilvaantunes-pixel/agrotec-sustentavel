be3340b2e6c229a25402ba82063c3045e77e518c
thaissilvaantunes-pixel <thais.silva.antunes@escola.pr.gov.br>
2026-06-10 14:24:30 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,31 @@
 
+function saibaMais() {
+    document.getElementById("producao").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function animarContador(id, valorFinal) {
+
+    let numero = 0;
+    const elemento = document.getElementById(id);
+
+    const intervalo = setInterval(() => {
+
+        numero += Math.ceil(valorFinal / 100);
+
+        if(numero >= valorFinal){
+            numero = valorFinal;
+            clearInterval(intervalo);
+        }
+
+        elemento.textContent = numero;
+
+    }, 20);
+}
+
+window.onload = () => {
+    animarContador("numero1", 500);
+    animarContador("numero2", 1200);
+    animarContador("numero3", 300);
+};
